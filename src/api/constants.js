55fa1cc90f8a3d7ce42cd1a945b1f110c5e0bb29
Lucas Vogel <contact@vogelino.ch>
2016-02-22 14:10:10 +0100

    Using process vars for private configs

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,19 +1,17 @@
-import config from 'config';
-
 module.exports = {
 	OPEN_NOTIFY: {
 		URL: 'http://api.open-notify.org/iss-now.json'
 	},
 	GEONAMES: {
 		URL: 'http://api.geonames.org/',
-		USERNAME: config.get('apiCredentials.geoNames.user')
+		USERNAME: process.env.GEONAME_USER
 	},
 	RESTCOUNTRIES: {
 		URL: 'https://restcountries.eu/rest/v1/name/__countryCode__?fullText=true'
 	},
 	FAROO: {
 		URL: `http://www.faroo.com/api?q=__countryName__&kwic=true&start=1&length=' +
-			'10&l=en&src=news&f=json&key=${config.get('apiCredentials.faroo.key')}`
+			'10&l=en&src=news&f=json&key=${process.env.FAROO_KEY}`
 	},
 	UPDATE_FREQUENCY: 1500,
 	DEMO_LAT_LNG: { latitude: -7.364855, longitude: -48.432910 } // Brasil
