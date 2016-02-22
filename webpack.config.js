065a7c32727ab5594a771dcfbc9c2a230871b8e5
Lucas Vogel <contact@vogelino.ch>
2016-02-22 12:40:57 +0100

    bundling styles with webpack rather than npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 var webpack = require('webpack');
 var path = require('path');
 var fs = require('fs');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var nib = require('nib');
 
 var rootDir = __dirname;
 var clientSrc = './src/client';
 var clientDest = './public';
 
-module.exports = {
+var config = {
 	context: rootDir,
 	entry: [
 		'babel-polyfill',
@@ -27,19 +29,32 @@ module.exports = {
 				}
 			},
 			{
+				test: /\.styl$/,
+				loader: ExtractTextPlugin.extract('style-loader', 'css-loader!stylus-loader')
+			},
+			{
 				test: /\.(jpg|png|gif|svg)$/,
 				loader: 'file-loader'
 			}
 		]
 	},
 	resolve: {
-		extensions: [ '', '.js', '.jpg', '.png', '.gif', '.json' ]
+		extensions: [ '', '.js', '.jpg', '.png', '.gif', '.json', '.styl' ]
 	},
 	plugins: [
-		new webpack.NoErrorsPlugin()
+		new webpack.NoErrorsPlugin(),
+		new ExtractTextPlugin('bundle.css', { allChunks: true })
 	],
 	stats: {
 		colors: true
 	},
-	devtool: 'source-map'
+	stylus: {
+		use: [ nib() ]
+	}
 };
+
+if (process.env.NODE_ENV === 'development') {
+	config.devtool = 'source-map';
+}
+
+module.exports = config;
